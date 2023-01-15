13fb890ef4e91967f7f91d6a5891739c4b558bcc
ThonAtaide <thon_ataide10@hotmail.com>
2023-01-15 01:04:57 -0300

    installed code style lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,21 @@
-
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.util.regex.Pattern.compile
 
+buildscript {
+    repositories {
+        maven("https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:11.0.0")
+    }
+}
+
 plugins {
-    id("org.springframework.boot") version "2.7.5"
-    id("io.spring.dependency-management") version "1.1.0"
-    id("org.jetbrains.kotlin.plugin.noarg") version "1.7.21"
-    kotlin("jvm") version "1.7.21"
     kotlin("plugin.spring") version "1.7.21"
+    kotlin("jvm") version "1.7.21"
+    id("org.jetbrains.kotlin.plugin.noarg") version "1.7.21"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("org.springframework.boot") version "2.7.5"
 }
 
 noArg {
@@ -34,8 +42,9 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-kotlin:1.6.0")
     implementation("org.springdoc:springdoc-openapi-data-rest:1.6.0")
 
-    implementation ("io.github.microutils:kotlin-logging-jvm:2.0.11")
-    implementation ("ch.qos.logback:logback-classic:1.2.6")
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
+    implementation("ch.qos.logback:logback-classic:1.2.6")
+    implementation("com.pinterest:ktlint:0.36.0")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -52,3 +61,5 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+apply(plugin = "org.jlleitschuh.gradle.ktlint")
